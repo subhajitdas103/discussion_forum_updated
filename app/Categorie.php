e308374225d8be2489822ca70b1f255e4858d833
subhajitdas103 <53931017+subhajitdas103@users.noreply.github.com>
2023-05-24 22:28:12 +0530

    Update Categorie.php

--- app/Categorie.php
+++ app/Categorie.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
     protected $guarded = [
-        'id', 'title', 'created_at', 'updated_at',
+        'id', 'title', 'created_at',
     ];
 }
